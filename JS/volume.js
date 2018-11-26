6cd422a25adde5ae56461e2432c374bc7eed356b
Jaacii <sliphno@hotmail.de>
2018-11-27 00:26:25 +0100

    test

--- JS/volume.js
+++ JS/volume.js
@@ -1,14 +1,59 @@
-var volumeslider;
+var sliders = document.getElementsByClassName("slider"),
+    filter = audioContext.createBiquadFilter();
+    
+audioElementSource2.connect(filter);
+filter.connect(audioContext.destination);
+
+//Slidern Eventlistener hinzufügen
+for (var i = 0; i < sliders.length; i++) {
+    sliders[i].addEventListener("mousemove", sliderchange);
+}
+
+
+function sliderchange() {
+    switch(this.id) {
+        case "volumeslider":
+            audioElement2.volume = this.value / 100;
+            document.getElementById("volumeOutput").innerHTML = this.value + " dB";
+            break;
+        case "gainslider":
+             filter.gain.value = this.value;
+            document.getElementById("gainOutput").innerHTML = this.value + " dB";
+            break;
+        case "qualityslider":
+            filter.Q.value = this.value;
+            document.getElementById("qualityOutput").innerHTML = this.value;
+            break;
+        
+    }
+}
+
+
+
+
+
+
+
 	
 	
-		volumeslider = document.getElementById("volumeslider");		//reagiert plötzlich nicht ?
-		volumeslider.addEventListener("mousemove", setvolume);
-		
+	
+	
+	
+	
+	
+	
+	
 
-			
-		
-		
-		function setvolume(){
-			audioElement2.volume = volumeslider.value / 100;
-		}
 
+	
+
+
+	
+	
+
+
+	
+
+		
+		
+		
