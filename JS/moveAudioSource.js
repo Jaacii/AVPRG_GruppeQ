05fb74b0bad43a9010d0f0739b40cd603e629f48
Jaacii <sliphno@hotmail.de>
2018-12-03 16:57:50 +0100

    koodinaten ausgabe fixed

--- JS/moveAudioSource.js
+++ JS/moveAudioSource.js
@@ -1,30 +1,27 @@
  	//MoveButtons     funktioniert scheinbar noch nicht
 		 
-		 var xDisplay =0, 
-			 yDisplay =0;			//die Floatzahl verursachte Anzeigefehler. Daher gibt es eine extra Variable
 		 function moveSource(direction) {
 		 
 		 
 		 if (direction === 0) {
 			
 			y = y+0.1;
-			yDisplay = yDisplay+1;
+			y = Math.round (y*100) / 100 
 			
 			document.getElementById("Ycoordinate").textContent= y ;
 			resonanceAudioScene.setListenerPosition(x, y, z);
 			}
 		else if (direction === 1) {
 			x = x+0.1;
-			xDisplay = x+1;	//Die X-Achse ist gespiegelt. Darum - statt +
+			x = Math.round (x*100) / 100 	//Zahl wird aufgerundet, damit keine 8 Stellige Zahl ausgegeben wird
 			
-			document.getElementById("Xcoordinate").textContent=  ((x*-1)*10)/10 ;
+			document.getElementById("Xcoordinate").textContent=  x*-1 ; //Die X-Achse ist gespiegelt. Darum * -1
 			resonanceAudioScene.setListenerPosition(x, y, z);
 			}
 		else if (direction === 2) {
 			x = 0.0;
 			y = 0.0;
-			xDisplay = 0; 
-			yDisplay =0;
+			
 			
 			document.getElementById("Xcoordinate").textContent= x;
 			document.getElementById("Ycoordinate").textContent=  y;
@@ -32,15 +29,15 @@
 			}
 		else if (direction === 3) {
 			x = x-0.1;
-			xDisplay + xDisplay - 1;
+			x = Math.round (x*100) / 100 
 			
-			document.getElementById("Xcoordinate").textContent= ((x*-1)*10)/10  ; 
+			document.getElementById("Xcoordinate").textContent= x*-1 ; 
 			resonanceAudioScene.setListenerPosition(x, y, z);
 			}
 		else if (direction === 4) {
 			
 			y = y-0.1;
-			yDisplay = yDisplay-1;
+			y = Math.round (y*100) / 100 
 			document.getElementById("Ycoordinate").textContent= y;
 			resonanceAudioScene.setListenerPosition(x, y, z);
 			
