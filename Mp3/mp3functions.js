222d8bed068336294735e7eb05241be1d00281a1
Jaacii <sliphno@hotmail.de>
2018-12-18 03:28:59 +0100

    großes update

--- Mp3/mp3functions.js
+++ Mp3/mp3functions.js
@@ -1,66 +1,64 @@
-<script>
+
 window.onload = function() {
-  
-  var file = document.getElementById("thefile");
+/*get html parts/elements*/
+  var file = document.getElementById("fileload");
   var audio = document.getElementById("audio");
   
+/*When audio selected -> create bars, audiocontext etc etc*/
   file.onchange = function() {
     var files = this.files;
     audio.src = URL.createObjectURL(files[0]);
     audio.load();
     audio.play();
-    var context = new AudioContext();
-    var src = context.createMediaElementSource(audio);
-    var analyser = context.createAnalyser();
+	
+    var audioContext = new AudioContext();
+    var audioElementSource2 = audioContext.createMediaElementSource(audio);
+    var analyser = audioContext.createAnalyser();
 
+	
     var canvas = document.getElementById("canvas");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     var ctx = canvas.getContext("2d");
 
-    src.connect(analyser);
-    analyser.connect(context.destination);
-
+    audioElementSource2.connect(analyser);
+    analyser.connect(audioContext.destination);
     analyser.fftSize = 256;
-
     var bufferLength = analyser.frequencyBinCount;
     console.log(bufferLength);
 
     var dataArray = new Uint8Array(bufferLength);
-
-    var WIDTH = canvas.width;
+    var WIDTH = canvas.width;					//HEIGHT -> canvas height etc
     var HEIGHT = canvas.height;
-
-    var barWidth = (WIDTH / bufferLength) * 2.5;
-    var barHeight;
+    var bWidth = (WIDTH / bufferLength) * 3;  // b -> bar height and width
+    var bHeight;
     var x = 0;
 
     function renderFrame() {
+/*AnalyserData + Canvas Colorisation*/
       requestAnimationFrame(renderFrame);
-
       x = 0;
-
       analyser.getByteFrequencyData(dataArray);
-
-      ctx.fillStyle = "#000";
+      ctx.fillStyle = "#000";	//black background
       ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
+	  
+/*Bars-Generation*/
       for (var i = 0; i < bufferLength; i++) {
-        barHeight = dataArray[i];
-        
-        var r = barHeight + (25 * (i/bufferLength));
-        var g = 250 * (i/bufferLength);
-        var b = 50;
-
+        bHeight = dataArray[i];
+		
+/*Color*/
+        var r = bHeight + (170 * (i/bufferLength));
+        var g = 450 * (i/bufferLength);
+        var b = 250;
         ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-        ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
-
-        x += barWidth + 1;
+        ctx.fillRect(x, HEIGHT - bHeight, bWidth, bHeight);
+        x += bWidth + 1;
       }
     }
 
+	
     audio.play();
     renderFrame();
   };
 };
-</script>
